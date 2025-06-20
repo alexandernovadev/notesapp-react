05ca3b844e3a930e5c8adf2213105aec4e96623c
alexandernovadev <alexsk88.dev@gmail.com>
2025-06-20 15:31:53 -0500

    Add ESLint configuration and update dependencies
    - Introduced .eslintrc.json and eslint.config.cjs for consistent linting rules based on Airbnb and Prettier standards.
    - Updated package.json to include ESLint and related plugins for improved code quality.
    - Changed Node version in .nvmrc from 16 to 22 for compatibility with new dependencies.

---
+++ eslint.config.cjs
@@ -0,0 +1,36 @@
+const { FlatCompat } = require('@eslint/eslintrc');
+const compat = new FlatCompat();
+
+module.exports = [
+  ...compat.extends('airbnb'),
+  ...compat.extends('airbnb/hooks'),
+  ...compat.extends('prettier'),
+  {
+    files: ["**/*.{js,jsx,ts,tsx}", "!node_modules"],
+    plugins: {
+      react: require("eslint-plugin-react"),
+      "react-hooks": require("eslint-plugin-react-hooks"),
+    },
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "module",
+      globals: {
+        window: "readonly",
+        document: "readonly",
+        navigator: "readonly",
+        jest: "readonly"
+      },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true
+        }
+      }
+    },
+    rules: {
+      "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
+      "react/react-in-jsx-scope": "off",
+      "import/no-extraneous-dependencies": ["error", { devDependencies: true }],
+      "react/prop-types": "off"
+    }
+  }
+]; 
